bd2a7b4f8cfee5c7fbdae3b7273bef334c9b0a2c
Josh Rolland <joshrolland@Joshs-MacBook-Pro.local>
2022-02-01 21:16:38 +0000

    added endpoint to controller

--- src/main/java/com/apps/wordee/controller/WordeeController.java
+++ src/main/java/com/apps/wordee/controller/WordeeController.java
@@ -1,7 +1,30 @@
 package com.apps.wordee.controller;
 
+import com.apps.wordee.processor.WordeeProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import org.apache.commons.io.FilenameUtils;
+
+@Validated
+@RestController
 public class WordeeController {
-//    add POST mapping
-//    check MultiPartFile originalFileName extension is txt, else throw exception
-//    call processor
+    @Autowired
+    WordeeProcessor wordeeProcessor;
+
+    @PostMapping("/analyseWords")
+    public ResponseEntity<String> analyseWords(@RequestParam("filePath") MultipartFile file) throws Exception {
+        if(!file.isEmpty() && FilenameUtils.getExtension(file.getOriginalFilename()).equals("txt")) {
+            return new ResponseEntity<>(wordeeProcessor.process(file), HttpStatus.OK);
+        } else {
+            throw new Exception("Error processing file");
+        }
+    }
+
 }
