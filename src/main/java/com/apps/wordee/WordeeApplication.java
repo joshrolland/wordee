558e57432d60ee3327c714131438dfe8967eae67
Josh Rolland <joshrolland@Joshs-MacBook-Pro.local>
2022-02-01 09:17:46 +0000

    created app skeleton

---
+++ src/main/java/com/apps/wordee/WordeeApplication.java
@@ -0,0 +1,13 @@
+package com.apps.wordee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apps/wordee/WordeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.wordee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
